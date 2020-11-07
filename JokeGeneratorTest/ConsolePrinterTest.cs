eca78b87be1fd85f7553075ea9337a3584175ddf
Rob Leibold <rob.leibold@gmail.com>
2020-11-06 21:57:43 -0500

    Added unit test for ConsolePrinter class.

--- JokeGeneratorTest/ConsolePrinterTest.cs
+++ JokeGeneratorTest/ConsolePrinterTest.cs
@@ -1,15 +1,20 @@
+using ConsoleApp1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JokeGeneratorTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ConsolePrinterTest1
     {
-        
+        private const string printValueTestValue = "Test123";       
         
         [TestMethod]
-        public void TestMethod1()
+        public void TestSetAndReturnConsolePrinter()
         {
+            var consolePrinter = new ConsolePrinter();
+            consolePrinter.Value(printValueTestValue);
+
+            Assert.AreEqual(printValueTestValue, ConsolePrinter.PrintValue);
         }
     }
 }

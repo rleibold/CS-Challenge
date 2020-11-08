8800d659854d71237df623bef51eb30b22a3dee3
Rob Leibold <rob.leibold@gmail.com>
2020-11-08 13:47:50 -0500

    Updated todo notes and added Obsolete label to deprecated methods in JsonFeed class.

--- ConsoleApp1/JsonFeed.cs
+++ ConsoleApp1/JsonFeed.cs
@@ -10,17 +10,21 @@ namespace ConsoleApp1
 {
     public class JsonFeed // Updated to public to allow for unit testing
     {
-        static string _url = ""; // TODO - Should not be static, should be private
+	    // TODO - Should not be static, and should be private, this will cause issues for multiple instances
+	    // running in the same application as they will all share the same _url variable  
+	    static string _url = "";  
 
         public JsonFeed() { }
-        public JsonFeed(string endpoint, int results) //TODO Extra parameter in constructor to be removed or implemented
+        public JsonFeed(string endpoint, int results) //TODO - results parameter in constructor to be removed or implemented
         {
             _url = endpoint;
         }
         
-		public static string[] GetRandomJokes(string firstname, string lastname, string category) // TODO- Does not need to be static
+        [Obsolete("GetRandomJokes method is deprecated.  Please use JokeGeneratorService.GetRandomJoke()")]
+        public static string[] GetRandomJokes(string firstname, string lastname, string category) // TODO- Does not need to be static
 		{
-			HttpClient client = new HttpClient(); // TODO - this could be injected into the constructor in order to help with mocking?
+			// TODO - this could be injected into the constructor in order to help with mocking
+			HttpClient client = new HttpClient();
 			client.BaseAddress = new Uri(_url);
 			string url = "jokes/random";
 			if (category != null)
@@ -32,11 +36,11 @@ namespace ConsoleApp1
 				url += category;
 			}
 
-            string joke = Task.FromResult(client.GetStringAsync(url).Result).Result; // TODO - redundant call to Result
+            string joke = Task.FromResult(client.GetStringAsync(url).Result).Result;
 
             if (firstname != null && lastname != null)
             {
-                int index = joke.IndexOf("Chuck Norris");
+                int index = joke.IndexOf("Chuck Norris"); // TODO - overly complicated, this could be simplified using .Replace() method
                 string firstPart = joke.Substring(0, index);
                 string secondPart = joke.Substring(0 + index + "Chuck Norris".Length, joke.Length - (index + "Chuck Norris".Length));
                 joke = firstPart + " " + firstname + " " + lastname + secondPart;
@@ -50,7 +54,8 @@ namespace ConsoleApp1
         /// </summary>
         /// <param name="client2"></param>
         /// <returns></returns>
-		public static dynamic Getnames() // TODO Does not need to be static, loosely typed return object could be error prone
+        [Obsolete("Getnames method is deprecated.  Please use JokeGeneratorService.GetRandomName()")]
+        public static dynamic Getnames() // TODO Does not need to be static, loosely typed return object could be error prone
 		{
 			// TODO Implement using: https://www.names.privserv.com/api/
 			// This method should not be part of this class and should be in it's own class
@@ -60,9 +65,9 @@ namespace ConsoleApp1
 			return JsonConvert.DeserializeObject<dynamic>(result);
 		}
 
-		public static string[] GetCategories() // TODO Does not need to be static
+		[Obsolete("GetCategories method is deprecated.  Please use JokeGeneratorService.GetCategories()")]
+        public static string[] GetCategories() // TODO Does not need to be static
 		{
-			// TODO - this method does not work correctly - needs to be fixed with /jokes/categories appended to url
 			HttpClient client = new HttpClient();
 			client.BaseAddress = new Uri(_url);
 

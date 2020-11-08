be169fe42a1f0d51da0ab47b625e1bd7fcbba049
Rob Leibold <rob.leibold@gmail.com>
2020-11-07 21:13:28 -0500

    Added new JokeGeneratorService to hold joke generation logic decoupling from the API calls in JsonFeed

---
+++ ConsoleApp1/Services/JokeGeneratorService.cs
@@ -0,0 +1,38 @@
+using System.Net.Http;
+using ConsoleApp1.ApiClients.ChuckNorrisIoApiClient;
+using ConsoleApp1.ApiClients.NamePrivServApiClient;
+
+namespace ConsoleApp1.Services
+{
+    /// <summary>
+    /// This class will be responsible for the joke generation logic
+    /// </summary>
+    public class JokeGeneratorService
+    {
+        private const string chuckNorrisName = "Chuck Norris";
+        
+        private ChuckNorrisIoApiClient chuckNorrisIoApiClient;
+        private NamePrivServApiClient namePrivServApiClient;
+        
+        public JokeGeneratorService(HttpClient httpClient, string chuckNorrisBaseUrl, string namePrivServBaseUrl)
+        {
+            chuckNorrisIoApiClient = new ChuckNorrisIoApiClient(httpClient, chuckNorrisBaseUrl);
+            namePrivServApiClient = new NamePrivServApiClient(httpClient, namePrivServBaseUrl);
+        }
+
+        public string GetRandomJoke(string firstname, string lastname, string category = null)
+        {
+            var chuckNorrisIoJokeDTO = chuckNorrisIoApiClient.GetRandomJokeAPI(category);
+
+            var jokeString = chuckNorrisIoJokeDTO.value;
+            if (firstname != null && lastname != null)
+            {
+                return jokeString.Replace(chuckNorrisName, firstname + " " + lastname);
+            }
+            else
+            {
+                return jokeString;
+            }
+        }
+    }
+}
